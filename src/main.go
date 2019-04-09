18279a086e9a533dcbde0dcd0eed543bdbabbaeb
Mr.neko <49114573+nekosekai@users.noreply.github.com>
2019-04-09 21:22:08 +0800

    No.7

--- src/main.go
+++ src/main.go
@@ -8,7 +8,11 @@ import (
 func main() {
 	var nums = []int{5, 7, 11, 20}
 	target := 16
-	var result []int
-	result = leetcode.TwoSum(nums, target)
-	fmt.Println(result)
+	var res1 []int
+	res1 = leetcode.TwoSum(nums, target)
+	fmt.Println(res1)
+	var x = 1534236469
+	var res2 int
+	res2 = leetcode.ReverseInteger(x)
+	fmt.Println(res2)
 }
