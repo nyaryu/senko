3820929a1cdeec39c8120881fabbbade17852d22
Mr.neko <49114573+nekosekai@users.noreply.github.com>
2019-04-14 19:28:05 +0800

    No.21

---
+++ src/leetcode/mergeTwoSortedLists.go
@@ -0,0 +1,20 @@
+package leetcode
+
+/*
+No.21
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+*/
+
+func MergeTwoSortedLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	return l1
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
