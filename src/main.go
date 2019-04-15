ff59fe3fc4715e53b874b713372039e0642c505b
Mr.neko <49114573+nekosekai@users.noreply.github.com>
2019-04-15 20:27:01 +0800

    No.27

--- src/main.go
+++ src/main.go
@@ -9,4 +9,8 @@ func main() {
 	x := "(])"
 	var res = leetcode.ValidParentheses(x)
 	fmt.Println(res)
+	y := []int{3, 2, 2, 3}
+	val := 3
+	var res1 = leetcode.RemoveElement(y, val)
+	fmt.Println(res1)
 }
