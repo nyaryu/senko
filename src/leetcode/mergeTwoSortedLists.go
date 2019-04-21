1d603646040377ed6b2e3a44293f6d5c74985820
Mr.neko <49114573+nekosekai@users.noreply.github.com>
2019-04-21 13:46:14 +0800

    No.21

--- src/leetcode/mergeTwoSortedLists.go
+++ src/leetcode/mergeTwoSortedLists.go
@@ -10,8 +10,27 @@ Output: 1->1->2->3->4->4
 */
 
 func MergeTwoSortedLists(l1 *ListNode, l2 *ListNode) *ListNode {
-
-	return l1
+	result := ListNode{}
+	temp := &result
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			temp.Next = l2
+			break
+		}
+		if l2 == nil {
+			temp.Next = l1
+			break
+		}
+		if l1.Val < l2.Val {
+			temp.Next = l1
+			l1 = l1.Next
+		} else {
+			temp.Next = l2
+			l2 = l2.Next
+		}
+		temp = temp.Next
+	}
+	return result.Next
 }
 
 type ListNode struct {
